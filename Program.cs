6416ffc8c7f37c5bf5d67713784a1eed455fe254
zxzc77 <71154926+zxzc77@users.noreply.github.com>
2020-09-11 18:32:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Short_story_console_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 40;
+            Console.Title = "An AdVeNtUrE";
+
+            Console.WriteLine("There once was a little bitch as- . . . a boy named...");
+            Console.ReadLine();
+            Console.WriteLine("... who decided to go shopping one day. \n Eventually he ending up buying a...");
+            Console.ReadLine();
+            Console.WriteLine("Unfortunately he didn't have the money to pay, so the cops were called. Thankfully he...");
+            Console.ReadLine();
+            Console.WriteLine("The end \n... \nWhat a gay story.");
+        }
+    }
+}
