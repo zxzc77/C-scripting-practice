74065496baf490512fc332633e4995fc8b080c0d
zxzc77 <71154926+zxzc77@users.noreply.github.com>
2020-09-11 18:37:35 -0300

    Add files via upload

--- Short story.cs
+++ Short story.cs
@@ -11,7 +11,7 @@ namespace Short_story_console_homework
 
             Console.WriteLine("There once was a little bitch as- . . . a boy named...");
             Console.ReadLine();
-            Console.WriteLine("... who decided to go shopping one day. \n Eventually he ending up buying a...");
+            Console.WriteLine("... who decided to go shopping one day. \n Eventually he ends up buying a...");
             Console.ReadLine();
             Console.WriteLine("Unfortunately he didn't have the money to pay, so the cops were called. Thankfully he...");
             Console.ReadLine();
