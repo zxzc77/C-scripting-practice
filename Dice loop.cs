436d51cd1da44009977ea9380581bff89b9a1e31
zxzc77 <71154926+zxzc77@users.noreply.github.com>
2020-09-11 22:39:01 -0300

    Add files via upload

---
+++ Dice loop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Loops_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int roll01 = 0;
+            int roll02 = 1;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice!");
+
+            while (roll01 != roll02)
+            {
+                Console.ReadKey();
+
+                roll01 = numberGen.Next(1, 7);
+                roll02 = numberGen.Next(1, 7);
+                Console.WriteLine("You rolled " + roll01);
+                Console.WriteLine("you rolled " + roll02 + "\n");
+                attempts++;
+
+            }
+
+            Console.WriteLine("It took you " + attempts + " attempts to roll doubles!");
+
+        }
+    }
+}
